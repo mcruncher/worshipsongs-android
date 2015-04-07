806809687c263ea232a803c17d79c84f28229ea4
Seenivasan <mailtonivass@gmail.com>
2015-04-07 21:42:21 +0800

    #110 Created the Tabs Page adapter class

---
+++ app/src/main/java/org/worshipsongs/adapter/TabsPagerAdapter.java
@@ -0,0 +1,13 @@
+package org.worshipsongs.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
+import org.worshipsongs.fragment.AuthorListFragment;
+import org.worshipsongs.fragment.SongBookListFragment;
+import org.worshipsongs.fragment.SongsListFragment;
+
+public class TabsPagerAdapter  {
+}
