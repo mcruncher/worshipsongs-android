951ce624c6d16d5c6198d1aae78e9580a7b07596
Seenivasan <mailtonivass@gmail.com>
2015-04-07 21:45:58 +0800

    #110 Override some needed method from Fragment Pager Adapter

--- app/src/main/java/org/worshipsongs/adapter/TabsPagerAdapter.java
+++ app/src/main/java/org/worshipsongs/adapter/TabsPagerAdapter.java
@@ -9,5 +9,30 @@ import org.worshipsongs.fragment.AuthorListFragment;
 import org.worshipsongs.fragment.SongBookListFragment;
 import org.worshipsongs.fragment.SongsListFragment;
 
-public class TabsPagerAdapter  {
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    /**
+     * The total number of tabs
+     */
+    private int totalTabs;
+
+    public TabsPagerAdapter(FragmentManager fm, int totalTabs) {
+        super(fm);
+        this.totalTabs = totalTabs;
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
 }
