ffc50a80391964aef4de7045018c682dfc06853e
Seenivasan <mailtonivass@gmail.com>
2015-04-07 21:31:21 +0800

    #110 Created tab fragment class file

---
+++ app/src/main/java/org/worshipsongs/fragment/TabFragment.java
@@ -0,0 +1,25 @@
+package org.worshipsongs.fragment;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost;
+
+import org.worshipsongs.adapter.TabsPagerAdapter;
+import org.worshipsongs.worship.R;
+
+/**
+ * Created by Seenivasan on 4/5/2015.
+ */
+public class TabFragment{
+}
