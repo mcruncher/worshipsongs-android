7fa7f2253e0270f8b0bcf63394c9650e5c274482
Seenivasan <mailtonivass@gmail.com>
2015-04-07 22:05:23 +0800

    #110 Updated the User Setting Activity

--- app/src/main/java/org/worshipsongs/activity/UserSettingActivity.java
+++ app/src/main/java/org/worshipsongs/activity/UserSettingActivity.java
@@ -1,9 +1,14 @@
 package org.worshipsongs.activity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.worshipsongs.fragment.SettingsPreferenceFragment;
+import org.worshipsongs.worship.R;
 
 /**
  * @Author : Seenivasan
@@ -11,14 +16,40 @@ import org.worshipsongs.fragment.SettingsPreferenceFragment;
  */
 public class UserSettingActivity extends PreferenceActivity
 {
+    private ActionBar actionBar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        actionBar = getActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Settings");
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsPreferenceFragment()).commit();
     }
 
     public void activityFinish() {
         finish();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
 }
 
