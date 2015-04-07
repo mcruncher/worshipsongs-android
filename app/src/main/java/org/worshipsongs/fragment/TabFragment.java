1d49d2f7f65123f58cd837a25e1a4849b2e112c7
Seenivasan <mailtonivass@gmail.com>
2015-04-07 21:53:08 +0800

    #110 Set On Page Change Listener for view pager in Tab Fragment

--- app/src/main/java/org/worshipsongs/fragment/TabFragment.java
+++ app/src/main/java/org/worshipsongs/fragment/TabFragment.java
@@ -60,6 +60,26 @@ public class TabFragment  extends Fragment implements ActionBar.TabListener {
             }
         });
 
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                Log.w(this.getClass().getSimpleName(), "Tab page position" +position);
+                tabHost.setCurrentTab(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
         return FragentLayout;
     }
 
