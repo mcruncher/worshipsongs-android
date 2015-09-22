0fd5fa77f4870453127f60667437418b40a607dd
Madasamy <swotge@gmail.com>
2015-09-22 21:50:36 +0800

    #168 UI enhancements in navigation drawer

--- app/src/main/java/org/worshipsongs/activity/NavigationDrawerActivity.java
+++ app/src/main/java/org/worshipsongs/activity/NavigationDrawerActivity.java
@@ -9,6 +9,7 @@ import org.worshipsongs.fragment.AboutWebViewFragment;
 import org.worshipsongs.fragment.HomeTabFragment;
 import org.worshipsongs.fragment.SettingsFragment;
 import org.worshipsongs.fragment.SettingsPreferenceFragment;
+import org.worshipsongs.worship.R;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 
@@ -21,9 +22,10 @@ public class NavigationDrawerActivity extends MaterialNavigationDrawer
     @Override
     public void init(Bundle bundle)
     {
-        this.addSection(newSection("Home", new HomeTabFragment()));
-        this.addSection(newSection("Settings", getSettingFragment()));
-        this.addSection(newSection("About", new AboutWebViewFragment()));
+        this.addSubheader("");
+        this.addSection(newSection("Home", R.drawable.ic_home_white, new HomeTabFragment()));
+        this.addSection(newSection("Settings", R.drawable.ic_settings_white, getSettingFragment()));
+        this.addSection(newSection("About", R.drawable.about, new AboutWebViewFragment()));
     }
 
     @NonNull
