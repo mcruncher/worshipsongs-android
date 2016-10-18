ba93c006129584fab397a01df752a2a05ee2c4cc
Madasamy <swotge@gmail.com>
2016-10-18 08:28:54 +0800

    Minor enhancements

--- bundle-db.sh
+++ bundle-db.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/crunchersaspire/worshipsongs-db-dev.git bundle-db
+echo "Copy file into assest dir"
 cp -rf bundle-db/songs.sqlite app/src/main/assets/
+ls -l app/src/main/assets
 rm -rf bundle-db
