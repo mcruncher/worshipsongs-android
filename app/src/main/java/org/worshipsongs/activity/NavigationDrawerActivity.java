38ab9ef6eff71d187a1e8529bf78b508c6e7cac7
swotge <swotge@gmail.com>
2017-03-01 16:38:55 +0800

    #275 Support to share app in social media

--- app/src/main/java/org/worshipsongs/activity/NavigationDrawerActivity.java
+++ app/src/main/java/org/worshipsongs/activity/NavigationDrawerActivity.java
@@ -3,9 +3,12 @@ package org.worshipsongs.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.worshipsongs.WorshipSongApplication;
 import org.worshipsongs.fragment.HomeTabFragment;
 import org.worshipsongs.service.PresentationScreenService;
+import org.worshipsongs.service.SongListAdapterService;
 import org.worshipsongs.worship.R;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
@@ -27,14 +30,16 @@ public class NavigationDrawerActivity extends MaterialNavigationDrawer
         this.addSection(newSection(getString(R.string.home), R.drawable.ic_library_books_white, new HomeTabFragment()));
         this.addSection(newSection(getString(R.string.settings), R.drawable.ic_settings_white, getSettings()));
         this.addSection(newSection(getString(R.string.rate_this_app), android.R.drawable.star_off, getPlayStore()));
+        this.addSection(newSection(getString(R.string.share), android.R.drawable.ic_menu_share, getShare()));
+
     }
 
-   private Intent getSettings()
+    private Intent getSettings()
     {
         return new Intent(NavigationDrawerActivity.this, UserSettingActivity.class);
     }
 
-   private Intent getPlayStore()
+    private Intent getPlayStore()
     {
         Uri uri = Uri.parse("market://details?id=" + this.getApplicationContext().getPackageName());
         Intent playStore = new Intent(Intent.ACTION_VIEW, uri);
@@ -47,6 +52,17 @@ public class NavigationDrawerActivity extends MaterialNavigationDrawer
         return (Intent.FLAG_ACTIVITY_NO_HISTORY |
                 Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
                 Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+    }
+
+    private Intent getShare()
+    {
+        Intent shareIntent = new Intent(Intent.CATEGORY_APP_EMAIL);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_info));
+        shareIntent.setType("text/plain");
+        Intent intent = Intent.createChooser(shareIntent, getString(R.string.share) + " " + getString(R.string.app_name) + " in");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return intent;
+
     }
 
     @Override
