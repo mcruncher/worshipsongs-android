2cd3d89e39aacededd371d8a804b2e8ad16caeb7
Madasamy <madasamy@mcruncher.com>
2017-10-05 11:42:15 +0800

    #439 Removed unused class

--- app/src/main/java/org/worshipsongs/adapter/ItemAdapter.java
+++ app/src/main/java/org/worshipsongs/adapter/ItemAdapter.java
@@ -1,22 +1,6 @@
-/*
- * Copyright 2014 Magnus Woxblom
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.worshipsongs.adapter;
 
-import android.support.v4.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +16,11 @@ import org.worshipsongs.domain.DragDrop;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * author:  Madasamy
+ * version: 4.x.x
+ */
 
 public class ItemAdapter extends DragItemAdapter<DragDrop, ItemAdapter.ViewHolder>
 {
