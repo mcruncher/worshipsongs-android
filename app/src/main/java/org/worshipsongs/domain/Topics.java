e2d2c1cbba48cc85ea1856ed2c5216de53e6b679
Madasamy <madasamy@mcruncher.com>
2017-10-12 08:50:54 +0800

    #441 Removed unused AbstractDomain

--- app/src/main/java/org/worshipsongs/domain/Topics.java
+++ app/src/main/java/org/worshipsongs/domain/Topics.java
@@ -8,15 +8,37 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
  * Version : 3.x
  */
 
-public class Topics extends AbstractDomain
+public class Topics
 {
-
+    private int id;
+    private String name;
     private String tamilName;
     private String defaultName;
+    private int noOfSongs;
 
     public Topics()
     {
         //Do nothing
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
     }
 
     public String getTamilName()
@@ -44,4 +66,24 @@ public class Topics extends AbstractDomain
         this.setName(name);
     }
 
+    public int getNoOfSongs()
+    {
+        return noOfSongs;
+    }
+
+    public void setNoOfSongs(int noOfSongs)
+    {
+        this.noOfSongs = noOfSongs;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Topics{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tamilName='" + tamilName + '\'' +
+                ", defaultName='" + defaultName + '\'' +
+                '}';
+    }
 }
