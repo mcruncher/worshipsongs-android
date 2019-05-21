e86b7b7b9688535f9a9bd811e3f073dc348f001f
Madasamy <madasamy@mcruncher.com>
2019-05-21 09:57:37 +0800

    #519 Fixed app crashing on android 7.x

--- app/src/main/java/org/worshipsongs/activity/AbstractAppCompactActivity.java
+++ app/src/main/java/org/worshipsongs/activity/AbstractAppCompactActivity.java
@@ -1,9 +1,17 @@
 package org.worshipsongs.activity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import org.worshipsongs.R;
 import org.worshipsongs.utils.ThemeUtils;
 
 /**
@@ -17,5 +25,37 @@ public class AbstractAppCompactActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         ThemeUtils.setTheme(this);
+    }
+
+    protected void setCustomActionBar()
+    {
+        if (getSupportActionBar() == null) {
+            setStatusBarColor();
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            toolbar.setVisibility(View.VISIBLE);
+            toolbar.setBackgroundColor(getToolbarColor());
+            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+                toolbar.setElevation(0);
+            }
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setStatusBarColor()
+    {
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.colorPrimaryDark, typedValue, true);
+        window.setStatusBarColor(typedValue.data);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private int getToolbarColor()
+    {
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.colorPrimary, typedValue, true);
+        return typedValue.data;
     }
 }
