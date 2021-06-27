73d600d09f3b96f98799a376ddbdd4c5d1f215a4
madasamys <swotge@gmail.com>
2021-06-27 09:04:29 +0530

    #558 Support to download service from dropbox

--- app/src/main/java/org/worshipsongs/fragment/LiveShareFragment.kt
+++ app/src/main/java/org/worshipsongs/fragment/LiveShareFragment.kt
@@ -1,41 +1,115 @@
 package org.worshipsongs.fragment
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import org.worshipsongs.CommonConstants
 import org.worshipsongs.R
+import org.worshipsongs.adapter.TitleAdapter
 import org.worshipsongs.listener.SongContentViewListener
 import org.worshipsongs.registry.ITabFragment
+import org.worshipsongs.task.AsyncLiveShareTask
+import org.worshipsongs.utils.UnzipUtils
+import java.util.ArrayList
 
-class LiveShareFragment : Fragment(), ITabFragment {
+class LiveShareFragment : Fragment(), ITabFragment, TitleAdapter.TitleAdapterListener<String>
+{
+    private var state: Parcelable? = null
+    private var services: MutableList<String> = ArrayList()
+    private var serviceListView: ListView? = null
+    private var titleAdapter: TitleAdapter<String>? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState != null)
+        {
+            state = savedInstanceState.getParcelable(CommonConstants.STATE_KEY)
+        }
         setHasOptionsMenu(true)
+        initSetup()
+    }
+
+    private fun initSetup()
+    {
+        AsyncLiveShareTask(activity as AppCompatActivity).execute()
+        var serviceDir = "/data/data/" + context!!.applicationContext.packageName + "/databases/service"
+        services = UnzipUtils.getServiceNames(serviceDir)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         val view = inflater.inflate(R.layout.songs_layout, container, false)
-      //  setInfoTextView(view)
-        //setListView(view)
+        //  setInfoTextView(view)
+        setListView(view)
         return view
+    }
+
+    private fun setListView(view: View)
+    {
+        serviceListView = view.findViewById<View>(R.id.song_list_view) as ListView
+        titleAdapter = TitleAdapter((activity as AppCompatActivity?)!!, R.layout.songs_layout)
+        titleAdapter!!.setTitleAdapterListener(this)
+        titleAdapter!!.addObjects(services)
+        serviceListView!!.adapter = titleAdapter
     }
 
     override val title: String
         get() = "live_share"
 
-    override fun defaultSortOrder(): Int {
+    override fun defaultSortOrder(): Int
+    {
         return 5
     }
 
-    override fun checked(): Boolean {
+    override fun checked(): Boolean
+    {
         return true
     }
 
-    override fun setListenerAndBundle(songContentViewListener: SongContentViewListener?, bundle: Bundle) {
+    override fun setListenerAndBundle(songContentViewListener: SongContentViewListener?, bundle: Bundle)
+    {
 
+    }
+
+    override fun setViews(objects: Map<String, Any>, text: String?)
+    {
+        val titleTextView = objects[CommonConstants.TITLE_KEY] as TextView?
+        titleTextView!!.text = text!!
+        //titleTextView.setOnClickListener(TextViewOnClickListener(text))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle)
+    {
+        if (this.isAdded && serviceListView !=null)
+        {
+            outState.putParcelable(CommonConstants.STATE_KEY, serviceListView!!.onSaveInstanceState())
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        initSetup()
+        refreshListView()
+    }
+
+    private fun refreshListView()
+    {
+        if (state != null)
+        {
+            serviceListView!!.onRestoreInstanceState(state)
+        } else if (titleAdapter != null)
+        {
+            titleAdapter!!.addObjects(services)
+//            infoTextView!!.visibility = if (services.isEmpty()) View.VISIBLE else View.GONE
+        }
     }
 }
---
+++ app/src/main/java/org/worshipsongs/task/AsyncLiveShareTask.kt
@@ -0,0 +1,139 @@
+package org.worshipsongs.task;
+
+import android.content.SharedPreferences
+import android.os.AsyncTask
+import android.preference.PreferenceManager
+import android.util.Log
+import android.view.ContextThemeWrapper
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity;
+import org.apache.commons.io.FileUtils
+import org.worshipsongs.CommonConstants
+import org.worshipsongs.R
+import org.worshipsongs.utils.UnzipUtils
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
+
+public class AsyncLiveShareTask(private val context: AppCompatActivity) : AsyncTask<String, Int, Boolean>()
+{
+    private var progressBar: ProgressBar? = null
+    private var destinationFile: File? = null
+    private var builder: AlertDialog.Builder? = null
+    private var alertDialog: AlertDialog? = null
+    private var progressTextView: TextView? = null
+    private val sharedPreferences: SharedPreferences
+
+    init
+    {
+        setDialogBuilder(context)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    }
+
+    private fun setDialogBuilder(context: AppCompatActivity)
+    {
+        builder = AlertDialog.Builder(ContextThemeWrapper(context, R.style.DialogTheme))
+        val li = LayoutInflater.from(context)
+        val view = li.inflate(R.layout.dialog_custom_title, null)
+        val titleTextView = view.findViewById<View>(R.id.title) as TextView
+        titleTextView.setText(R.string.download_song_database)
+        val textView = view.findViewById<View>(R.id.subtitle) as TextView
+        textView.visibility = View.GONE
+        progressBar = view.findViewById<View>(R.id.progressBar2) as ProgressBar
+        progressBar!!.visibility = View.VISIBLE
+
+        val progressInitialTextSize = view.findViewById<View>(R.id.initial_text_view) as TextView
+        progressInitialTextSize.visibility = View.VISIBLE
+
+        progressTextView = view.findViewById<View>(R.id.progress_text_view) as TextView
+        progressTextView!!.visibility = View.VISIBLE
+        builder!!.setView(view)
+    }
+
+    override fun onPreExecute()
+    {
+        alertDialog = builder!!.create()
+        alertDialog!!.show()
+    }
+
+    override fun doInBackground(vararg strings: String): Boolean?
+    {
+        try
+        {
+            var count: Int
+            destinationFile = File(context.cacheDir.absolutePath, "liveshare")
+            val remoteUrl = sharedPreferences.getString(CommonConstants.LIVE_SHARE_PATH_KEY, "")
+            val url = URL(remoteUrl)
+            val connection = url.openConnection()
+            connection.readTimeout = 60000
+            connection.connectTimeout = 60000
+            connection.connect()
+            val lengthOfFile = connection.contentLength
+            val input = BufferedInputStream(url.openStream(), 10 * 1024)
+            // Output stream to write file in SD card
+            val output = FileOutputStream(destinationFile!!)
+            val data = ByteArray(1024)
+            var total: Long = 0
+            publishProgress(0)
+            Log.e(this.javaClass.simpleName, "Length of file $lengthOfFile")
+            while (input.read(data).let {
+                        count = it; it != -1
+                    }) {
+                total += count.toLong()
+                output.write(data, 0, count)
+                Log.e(this.javaClass.simpleName, "publish")
+                publishProgress((total * 100 / lengthOfFile).toInt())
+            }
+            output.flush()
+            output.close()
+            input.close()
+            return true
+        } catch (ex: Exception)
+        {
+            Log.e(this.javaClass.simpleName, "Error", ex)
+            return false
+        }
+
+    }
+
+    override fun onProgressUpdate(vararg values: Int?)
+    {
+        super.onProgressUpdate(*values)
+        progressBar!!.progress = values[0]!!
+        progressTextView!!.text = values[0].toString() + "/100"
+    }
+
+    override fun onPostExecute(successfull: Boolean?)
+    {
+        try
+        {
+            alertDialog!!.cancel()
+            if (successfull!!)
+            {
+                Log.i(AsyncDownloadTask::class.java.simpleName, "Remote database copied successfully.")
+                var tempLiveShareDir = File(context.cacheDir.absolutePath, "tempLiveShare")
+                UnzipUtils.unzip(destinationFile!!, tempLiveShareDir, false)
+                var tempServiceDir = File(context.cacheDir.absolutePath, "tempService")
+                UnzipUtils.unZipServices(tempLiveShareDir, tempServiceDir)
+                var serviceDir = "/data/data/" + context.applicationContext.packageName + "/databases/service"
+                FileUtils.copyDirectory(tempServiceDir, File(serviceDir))
+                FileUtils.deleteQuietly(tempLiveShareDir)
+                FileUtils.deleteQuietly(tempServiceDir)
+                Log.i(AsyncDownloadTask::class.java.simpleName, "Services ${UnzipUtils.getServiceNames(serviceDir)}")
+            }
+        } catch (ex: Exception)
+        {
+            Log.e(AsyncDownloadTask::class.java.simpleName, "Error")
+        } finally
+        {
+            FileUtils.deleteQuietly(destinationFile)
+        }
+    }
+
+}
+
