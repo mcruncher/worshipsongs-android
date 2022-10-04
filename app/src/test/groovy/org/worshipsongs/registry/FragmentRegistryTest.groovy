37ce8d5454dc769b029907a2162cf54d4e8827fc
James <james@mcruncher.com>
2022-10-04 16:35:12 +0800

    #573 Uncommented the test.

--- app/src/test/groovy/org/worshipsongs/registry/FragmentRegistryTest.groovy
+++ app/src/test/groovy/org/worshipsongs/registry/FragmentRegistryTest.groovy
@@ -11,33 +11,33 @@ import spock.lang.Specification
 @Ignore
 class FragmentRegistryTest extends Specification
 {
-//    def fragmentRegistry = new FragmentRegistry();
-//    def activity = Robolectric.buildActivity(FragmentActivity.class).create().start().resume().get()
-//
-//    def "Get default titles"()
-//    {
-//
-//        expect:
-//        fragmentRegistry.getTitles(activity).size() == 0
-//    }
-//
-//    def "Get tab Fragment"()
-//    {
-//        expect:
-//        fragmentRegistry.getTabFragment(DummyFragment1.class).defaultSortOrder() == 1
-//    }
-//
-//    def "Sort fragment"()
-//    {
-//        given:
-//        List<ITabFragment> fragmentList = new ArrayList<>();
-//        fragmentList.add(new DummyFragment2())
-//        fragmentList.add(new DummyFragment1())
-//
-//        when:
-//        Collections.sort(fragmentList,  new FragmentRegistry.TabFragmentComparator(fragmentRegistry))
-//
-//        then:
-//        fragmentList[0].class.getSimpleName() == "DummyFragment1"
-//    }
+    def fragmentRegistry = new FragmentRegistry();
+    def activity = Robolectric.buildActivity(FragmentActivity.class).create().start().resume().get()
+
+    def "Get default titles"()
+    {
+
+        expect:
+        fragmentRegistry.getTitles(activity).size() == 0
+    }
+
+    def "Get tab Fragment"()
+    {
+        expect:
+        fragmentRegistry.getTabFragment(DummyFragment1.class).defaultSortOrder() == 1
+    }
+
+    def "Sort fragment"()
+    {
+        given:
+        List<ITabFragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new DummyFragment2())
+        fragmentList.add(new DummyFragment1())
+
+        when:
+        Collections.sort(fragmentList,  new FragmentRegistry.TabFragmentComparator(fragmentRegistry))
+
+        then:
+        fragmentList[0].class.getSimpleName() == "DummyFragment1"
+    }
 }
